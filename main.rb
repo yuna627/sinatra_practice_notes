7704f60306a1266a528cd51ba1c175455418f9a9
Yuna Shimizu <h.yuina627@gmail.com>
2020-09-13 00:38:19 -0700

    Add comments for each url

--- main.rb
+++ main.rb
@@ -3,23 +3,32 @@ require 'sinatra/reloader'
 require 'json'
 require 'logger'
 
-logger = Logger.new('sinatra.log')
+LOGFILE = 'sinatra.log'.freeze
+DATAFILE = 'data/notes.json'.freeze
+
+logger = Logger.new(LOGFILE)
+
+# データの書き込み
 def write_data(data)
-  File.open('data/notes.json', 'w') do |note_file|
+  File.open(DATAFILE, 'w') do |note_file|
     JSON.dump(data, note_file)
   end
 end
 
+# データの読み込み
+# データが存在しないときは初期データを返す
 def load_data
   data = { 'last_id' => 0, 'notes' => {} }
-  return data unless File.exist?('data/notes.json')
+  return data unless File.exist?(DATAFILE)
 
-  File.open('data/notes.json') do |note_file|
+  File.open(DATAFILE) do |note_file|
     data = JSON.load(note_file)
   end
   data
 end
 
+# トップ画面
+# ノート一覧
 get '/notes' do
   @title = 'メモ'
   data = load_data
@@ -27,25 +36,28 @@ get '/notes' do
   erb :notes
 end
 
+# メモ新規投稿画面
 get '/notes/new' do
   @title = '新規投稿'
   erb :notes_new
 end
 
+# メモの新規投稿処理
 post '/notes' do
-  @data = load_data
-  @data['last_id'] += 1
-  @data['notes'][@data['last_id'].to_s] = {
-    id: @data['last_id'],
+  data = load_data
+  data['last_id'] += 1
+  data['notes'][data['last_id'].to_s] = {
+    id: data['last_id'],
     title: params[:note_title],
     body: params[:note_body]
   }
 
-  write_data(@data)
+  write_data(data)
 
   redirect to('/notes')
 end
 
+# メモ詳細画面
 get '/notes/:id' do
   @title = 'メモ詳細'
   data = load_data
@@ -53,6 +65,7 @@ get '/notes/:id' do
   erb :notes_detail
 end
 
+# メモの削除処理
 delete '/notes/:id' do
   data = load_data
   deleted = data['notes'].delete(params[:id].to_s)
@@ -62,13 +75,15 @@ delete '/notes/:id' do
   redirect to('/notes')
 end
 
+# メモの編集画面
 get '/notes/:id/edit' do
-  @title = 'メモ編集画面'
+  @title = 'メモ編集'
   data = load_data
   @note = data['notes'][params[:id]]
   erb :notes_edit
 end
 
+# メモの編集処理
 patch '/notes/:id' do
   data = load_data
   data['notes'][params[:id]]['title'] = params[:note_title]
