55586b80f5b9d401603839413413a7cbf457e63a
Yuna Shimizu <h.yuina627@gmail.com>
2020-09-12 23:31:54 -0700

    Refactor loading and writing data

--- main.rb
+++ main.rb
@@ -4,39 +4,36 @@ require 'json'
 require 'logger'
 
 logger = Logger.new('sinatra.log')
+def write_data(data)
+  File.open('data/notes.json', 'w') do |note_file|
+    JSON.dump(data, note_file)
+  end
+end
+
+def load_data
+  data = { 'last_id' => 0, 'notes' => {} }
+  return data unless File.exist?('data/notes.json')
+
+  File.open('data/notes.json') do |note_file|
+    data = JSON.load(note_file)
+  end
+  data
+end
 
 get '/notes' do
-  @title = 'Notes'
-  if File.exist?('data/notes.json')
-    File.open('data/notes.json') do |note_file|
-      note = JSON.load(note_file)
-      p note
-      @notes = note['notes']
-    end
-  else
-    @notes = {}
-  end
-
+  @title = 'メモ'
+  data = load_data
+  @notes = data['notes']
   erb :notes
 end
 
 get '/notes/new' do
-  @title = 'main'
+  @title = '新規投稿'
   erb :notes_new
 end
 
 post '/notes' do
-  @title = 'main'
-  # ファイルの読み込み(データがある場合)
-  if File.exist?('data/notes.json')
-    File.open('data/notes.json') do |note_file|
-      @data = JSON.load(note_file)
-    end
-  else
-    # データファイルの新規作成
-    @data = { 'last_id' => 0, 'notes' => {} }
-  end
-  # データの追加
+  @data = load_data
   @data['last_id'] += 1
   @data['notes'][@data['last_id'].to_s] = {
     id: @data['last_id'],
@@ -44,59 +41,39 @@ post '/notes' do
     body: params[:note_body]
   }
 
-  # 書き込み
-  File.open('data/notes.json', 'w') do |note_file|
-    JSON.dump(@data, note_file)
-  end
+  write_data(@data)
+
   redirect to('/notes')
 end
 
 get '/notes/:id' do
-  @title = 'edit'
-  # ファイルの読み込み
-  File.open('data/notes.json') do |note_file|
-    @data = JSON.load(note_file)
-    @note = @data['notes'][params[:id]]
-  end
+  @title = 'メモ詳細'
+  data = load_data
+  @note = data['notes'][params[:id]]
   erb :notes_detail
 end
 
 delete '/notes/:id' do
-  @title = 'main'
-  File.open('data/notes.json') do |note_file|
-    @data = JSON.load(note_file)
-    deleted = @data['notes'].delete(params[:id].to_s)
-    logger.info(msg: 'Deleted', note: deleted)
-  end
-  # 書き込み
-  File.open('data/notes.json', 'w') do |note_file|
-    JSON.dump(@data, note_file)
-  end
+  data = load_data
+  deleted = data['notes'].delete(params[:id].to_s)
+  logger.info(msg: 'Deleted', note: deleted)
+  write_data(data)
   # リダイレクトしてメモアプリトップにいく
   redirect to('/notes')
 end
 
 get '/notes/:id/edit' do
-  @title = 'main'
-  # ファイルの読み込み
-  File.open('data/notes.json') do |note_file|
-    @data = JSON.load(note_file)
-    @note = @data['notes'][params[:id]]
-  end
+  @title = 'メモ編集画面'
+  data = load_data
+  @note = data['notes'][params[:id]]
   erb :notes_edit
 end
 
 patch '/notes/:id' do
-  @title = 'main'
-  File.open('data/notes.json') do |note_file|
-    @data = JSON.load(note_file)
-    @data['notes'][params[:id]]['title'] = params[:note_title]
-    @data['notes'][params[:id]]['body'] = params[:note_body]
-  end
-  # 書き込み
-  File.open('data/notes.json', 'w') do |note_file|
-    JSON.dump(@data, note_file)
-  end
+  data = load_data
+  data['notes'][params[:id]]['title'] = params[:note_title]
+  data['notes'][params[:id]]['body'] = params[:note_body]
+  write_data(data)
   # リダイレクトしてshow memoにいく
   redirect to("/notes/#{params[:id]}")
 end
