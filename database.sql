33dcbdd09ee2b5987d567bd85ee4c0319f2698e4
Yuna Shimizu <h.yuina627@gmail.com>
2020-09-16 23:46:39 -0700

    Create DB schema

---
+++ database.sql
@@ -0,0 +1,10 @@
+-- メモ
+CREATE TABlE notes
+(
+  id SERIAL NOT NULL,
+  title VARCHAR NOT NULL,
+  body VARCHAR NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
+);
